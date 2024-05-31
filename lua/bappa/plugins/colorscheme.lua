dd2c4106f7860f735a2d0e38f850535c32ce6a24
dipanja <worldofbappa@gmail.com>
2024-06-01 02:42:09 +0530

    colorscheme setup function
    added some options which are from tokyonight repo

--- lua/bappa/plugins/colorscheme.lua
+++ lua/bappa/plugins/colorscheme.lua
@@ -6,11 +6,18 @@ return {
 	config = function()
 		require("tokyonight").setup({
 			style = "night",
+			-- style = "dark",
 			transparent = true, -- set transparent background
 			styles = {
 				sidebars = "transparent", -- set transparent sidebars
 				floats = "transparent",
+				comments = { italic = true },
+				keyworgs = { italic = true },
 			},
+			day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+			hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+			dim_inactive = true, -- dims inactive windows
+			lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
 		})
 		vim.cmd("colorscheme tokyonight")
 	end,
