d3d3dce8e86f4993e7b3a3e55ff062625194436f
dipanja <worldofbappa@gmail.com>
2024-06-01 18:01:42 +0530

    feat: markdown-preview.nvim added

---
+++ lua/bappa/plugins/markdown-preview.lua
@@ -0,0 +1,16 @@
+return {
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	ft = { "markdown" },
+	build = function()
+		vim.fn["mkdp#util#install"]()
+	end,
+	config = function()
+		vim.keymap.set(
+			"n",
+			"<leader>mm",
+			"<cmd>MarkdownPreviewToggle<CR>",
+			{ desc = "Preview markdown files in browser" }
+		)
+	end,
+}
