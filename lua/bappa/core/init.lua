4800ec70d78de8e8837f1a703b8364bc961e53f3
dipanja <worldofbappa@gmail.com>
2024-06-08 02:15:18 +0530

    feat: function toggle_highlight
    added toggle_highlight on search function

--- lua/bappa/core/init.lua
+++ lua/bappa/core/init.lua
@@ -1,2 +1,11 @@
 require("bappa.core.options")
 require("bappa.core.keymaps")
+_G.toggle_highlight = function()
+	if vim.o.hlsearch then
+		vim.o.hlsearch = false
+		print("Highlight search turned off")
+	else
+		vim.o.hlsearch = true
+		print("Highlight search turned on")
+	end
+end
