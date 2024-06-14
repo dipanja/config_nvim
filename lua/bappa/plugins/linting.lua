bb899898a472e2792a74a5c45fa5b3b4a78078ab
dipanja <worldofbappa@gmail.com>
2024-06-15 02:22:17 +0530

    added pydocstyle as linter

--- lua/bappa/plugins/linting.lua
+++ lua/bappa/plugins/linting.lua
@@ -10,8 +10,8 @@ return {
 			javascriptreact = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
 			svelte = { "eslint_d" },
-			-- python = { "pylint" },
-			python = { "flake8" },
+			-- python = { "pylint", "pydocstyle" },
+			python = { "flake8", "pydocstyle" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
