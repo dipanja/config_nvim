4ba97092dfad8c8a8120f38b68e7b530e6c3c813
dipanja <worldofbappa@gmail.com>
2024-09-04 01:13:56 +0530

    markdown preview

--- lua/bappa/plugins/markdown-preview.lua
+++ lua/bappa/plugins/markdown-preview.lua
@@ -1,6 +1,5 @@
 return {
 	"iamcco/markdown-preview.nvim",
-	event = "VeryLazy",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	ft = { "markdown" },
 	build = function()
