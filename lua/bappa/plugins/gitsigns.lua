c1056c9ff13ccf82d84bcbab2165cf5bb2f274db
dipanja <worldofbappa@gmail.com>
2024-09-30 04:45:05 +0530

    WIP

--- lua/bappa/plugins/gitsigns.lua
+++ lua/bappa/plugins/gitsigns.lua
@@ -42,16 +42,21 @@ return {
 				-- Actions
 				map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "stage hunk" })
 				map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "reset hunk" })
+
 				map("v", "<leader>hs", function()
 					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
 				end, { desc = "stage hunk" })
+
+				map("n", "<leader>hu", gitsigns.undo_stage_hunk, { desc = "undo stage hunk" })
+
 				map("v", "<leader>hr", function()
 					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
 				end, { desc = "reset hunk" })
-				map("n", "<leader>hS", gitsigns.stage_buffer)
-				map("n", "<leader>hu", gitsigns.undo_stage_hunk)
-				map("n", "<leader>hR", gitsigns.reset_buffer)
-				map("n", "<leader>hp", gitsigns.preview_hunk)
+
+				map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "stage buffer" })
+				map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "reset buffer" })
+
+				map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "preview hunk" })
 				map("n", "<leader>hb", function()
 					gitsigns.blame_line({ full = true })
 				end)
